17144afd6394f2ba3ddc68a93c7eef557e50d8eb
Daniel Khalil <dkses.hef@uea.edu.br>
2023-02-16 22:06:28 -0300

    Criando o projeto front-end do sistema de clientes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,16 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HelloComponent } from './hello.component';
+import { CursoComponent } from './curso/curso.component';
+import { ClientesComponent } from './clientes/clientes.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HelloComponent,
+    CursoComponent,
+    ClientesComponent
   ],
   imports: [
     BrowserModule,
